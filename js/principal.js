aefef45f0a028996056fa1f5d0adad0c075eebe7
FuentesRichard <richardfuentesrufino@gmail.com>
2021-08-11 14:17:53 -0300

    Atualizando imc pelo js

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,43 @@
-console.log("Fui carregado de um arquivo externo");
-
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Richard Nutricionista";
+
+//querySelecorAll retorna toda array
+var pacientes = document.querySelectorAll(".paciente");
+/*var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdImc = paciente.querySelector(".info-imc");*/
+
+
+for (var i = 0; i < pacientes.length; i++) {
+    var tdPeso = pacientes[i].querySelector(".info-peso");
+    var tdAltura = pacientes[i].querySelector(".info-altura");
+    var tdImc = pacientes[i].querySelector(".info-imc");
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso < 0 || peso > 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdPeso.textContent = "Peso inválido";
+    }
+
+    if (altura < 0 || altura > 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdAltura.textContent = "Altura inválida!";
+    }
+
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
+
+}
+
+
+
+
+//Segundo paciente
