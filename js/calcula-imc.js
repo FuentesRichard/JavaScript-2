a54f8b7b734ce791446f3ddfe2bac28730260762
FuentesRichard <richardfuentesrufino@gmail.com>
2021-08-12 11:31:19 -0300

    Separando codigo

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -3,10 +3,7 @@ titulo.textContent = "Richard Nutricionista";
 
 //querySelecorAll retorna toda array
 var pacientes = document.querySelectorAll(".paciente");
-/*var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
-var tdImc = paciente.querySelector(".info-imc");*/
-
+var paciente = document.querySelector(".paciente");
 
 for (var i = 0; i < pacientes.length; i++) {
     var tdPeso = pacientes[i].querySelector(".info-peso");
@@ -20,24 +17,26 @@ for (var i = 0; i < pacientes.length; i++) {
     if (peso < 0 || peso > 1000) {
         console.log("Peso inválido!");
         pesoEhValido = false;
-        tdPeso.textContent = "Peso inválido";
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
     }
 
     if (altura < 0 || altura > 3.00) {
         console.log("Altura inválida!");
         alturaEhValida = false;
-        tdAltura.textContent = "Altura inválida!";
+        tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
     }
 
     if (alturaEhValida && pesoEhValido) {
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
     }
-
-
 }
 
+function calculaImc(peso, altura) {
+    var imc = 0; 
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
 
-
-
-//Segundo paciente
---
+++ js/form.js
@@ -0,0 +1,38 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+    // Mexendo no form
+    //pegando minha form
+    var form = document.querySelector("#form-adiciona");
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    
+    //criando nova tr e td no form
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+    
+    //instanciando minha nova tr\td no form
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = calculaImc(peso, altura);
+    
+    // Colocando td na tr
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    console.log(pacienteTr);
+    
+    tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+});
